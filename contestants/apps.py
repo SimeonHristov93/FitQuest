3289997f8987ff466b4f4a5fa07aadc3a81d3359
Simeon Hristov <simeon.hristov@yahoo.com>
2026-04-05 15:46:20 +0100

    chore: signals edit

--- contestants/apps.py
+++ contestants/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class ContestantsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'contestants'
+
+    def ready(self):
+        from contestants import signals  # noqa: F401
--- contestants/signals.py
+++ contestants/signals.py
@@ -1,10 +1,25 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import ProgressEntry
+from notifications.utils import create_notification
+
+from .models import ProgressEntry, ChallengeContestant
 from leaderboard.utils import update_leaderboard
 
 
 @receiver(post_save, sender=ProgressEntry)
 def refresh_leaderboard_on_progress(sender, instance, **kwargs):
     update_leaderboard(instance.contestant.challenge)
+
+
+@receiver(post_save, sender=ChallengeContestant)
+def notify_on_challenge_join(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    create_notification(
+        user=instance.user,
+        title=f"Joined challenge: {instance.challenge.title}",
+        message=f"You are now participating in '{instance.challenge.title}'.",
+        challenge=instance.challenge,
+    )
